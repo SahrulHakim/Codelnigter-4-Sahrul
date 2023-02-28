bc716486d3147ab1c22f43af8af0a3a7ed8e9804
SahrulHakim <sahrulhakim895@gmail.com>
2023-02-28 10:04:56 +0700

    first commit

---
+++ app/Controllers/Page.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        echo view("about");
+    }
+    public function contact()
+                    {
+     $data['name'] = "Sahrul Hakim";
+    echo view("contact", $data);
+    }
+    public function faqs()
+    {
+ $data['data_faqs'] = [
+                    [
+                                        'question' => 'Apa itu Codeigniter?',
+                                        'answer' => 'Codeigniter adalah framework untuk membuat web'
+                    ],
+                    [
+                                        'question' => 'Siapa yang membuat Codeiginter?',
+                                        'answer' => 'CI awalnya dibuat oleh Ellislab'
+                    ],
+                    [
+                                        'question' => 'Codeigniter versi berapakah yang digunakan pada tutoril ini?',
+                                        'answer' => 'Codeigniter versi 4.0.4'
+                    ]
+];
+
+
+         echo view("faqs", $data);
+    }
+}
